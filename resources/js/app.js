edf3c27a982b50d2e737e2662eb73ecb14df90e6
iamratul <ratulsikder104@gmail.com>
2020-08-08 11:15:39 +0600

    STIKY NAVBAR

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+   
+   //sticky menu
+   $('.js--services-section').waypoint(function(direction){
+      if(direction == 'down'){
+         $('nav').addClass("sticky");
+      }else{
+         $('nav').removeClass("sticky");
+         
+      }
+   });
+   
+   // mixitup
    let mixer = mixitup(".container");
 
 });
